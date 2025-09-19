09a156b15115493d47fc0b394b0013bfc7e429d1
Vasyl Boliukh <vskesha@gmail.com>
2025-09-19 13:49:48 +0300

    Change settings for testing

--- config/settings/test.py
+++ config/settings/test.py
@@ -9,7 +9,10 @@ load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost",
+).split(",")
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
