b3ff1a974efee13fee9d7548f71f0f304e33b96a
Vasyl Boliukh <vskesha@gmail.com>
2025-09-19 13:24:54 +0300

    Initialize empty apps books, borrowings, notifications, payments, users

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "debug_toolbar",
+    "books",
+    "borrowings",
+    "notifications",
+    "payments",
+    "users",
 ]
 
 MIDDLEWARE = [
