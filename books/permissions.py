4fa860115d2af03bf4924e8f8798069bba2d3923
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-20 21:26:10 +0300

    fixed book permissions

--- books/permissions.py
+++ books/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.action == "list":
+        if view.action == "list":
             return True
         elif request.method in SAFE_METHODS:
             return request.user and request.user.is_authenticated
