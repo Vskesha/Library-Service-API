39ae582114b495d3e45c9cdc0a9d32800858d3d2
Vasyl Boliukh <vskesha@gmail.com>
2025-09-20 04:11:20 +0300

    Created endpoints for user

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+api_urlpatterns = [
+    path("users/", include("users.urls", namespace="users")),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/", include(api_urlpatterns)),
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
