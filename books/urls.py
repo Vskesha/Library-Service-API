02f19cce9c5a032154d301d9cc9c37fd221578cf
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-20 21:04:12 +0300

    fixed everything

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include, path
 from rest_framework import routers
 
 from books.views import BookViewSet
@@ -5,5 +6,9 @@ from books.views import BookViewSet
 router = routers.DefaultRouter()
 
 router.register("books", BookViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "books"
