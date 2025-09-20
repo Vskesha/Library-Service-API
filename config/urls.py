a41851f24dbe3e4ed4c45394a4a89d09943e6b37
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-20 21:05:59 +0300

    fixed book urls path

--- config/urls.py
+++ config/urls.py
@@ -11,7 +11,7 @@ from drf_spectacular.views import (
 
 api_urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
-    path("books/", include("book.urls", namespace="books")),
+    path("books/", include("books.urls", namespace="books")),
 ]
 
 urlpatterns = [
