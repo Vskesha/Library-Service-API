79bdb24f3206d20e5939ecbb0243d8b95ec36fc8
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-20 12:54:40 +0300

    updated permission class and added authetication and permission classes to bookviewset

--- books/permissions.py
+++ books/permissions.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.action == "list":
             return True
+        elif request.method in SAFE_METHODS:
+            return request.user and request.user.is_authenticated
         return request.user and request.user.is_staff
--- books/views.py
+++ books/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    authentication_classes = (JWTAuthentication, )
+    permission_classes = (IsAdminOrReadOnly, )
