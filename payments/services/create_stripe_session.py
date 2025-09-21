4927f035ba02dd79fef769ff42ecc97ff510493f
Tamerlan <egtamerlan@gmail.com>
2025-09-21 14:52:16 +0200

    created stripe session

---
+++ payments/services/create_stripe_session.py
@@ -0,0 +1,46 @@
+import stripe
+from django.conf import settings
+
+from payments.models import Payment
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def create_stripe_payment_session(borrowing):
+    book = borrowing.book
+    days = (borrowing.expected_return_date - borrowing.borrow_date).days
+    total_price = int(book.daily_fee * days * 100)
+
+    try:
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {
+                            "name": f"Borrowing a book: {book.title}",
+                        },
+                        "unit_amount": total_price,
+                    },
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url="http://localhost:8000/payments/success/",
+            cancel_url="http://localhost:8000/payments/cancel/",
+        )
+
+        Payment.objects.create(
+            status="Pending",
+            type="Payment",
+            borrowing=borrowing,
+            session_url=session.url,
+            session_id=session.id,
+            money_to_pay=total_price / 100,
+        )
+
+        return session
+
+    except stripe.error.StripeError as e:
+        raise Exception(f"Error creating stripe session: {str(e)}")
