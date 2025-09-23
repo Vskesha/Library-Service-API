ac44983044805590fe4720b433b9b5266ecd7cbd
Vadym <kirilkovadim193@gmail.com>
2025-09-23 13:32:50 +0300

    Make notify feature async

--- borrowings/signals.py
+++ borrowings/signals.py
@@ -2,9 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from borrowings.models import Borrowing
-from borrowings.services.borrowing_notification_service import (
-    BorrowingNotificationService,
-)
+from notifications.tasks import send_borrowing_notification_task
 from payments.models import Payment
 from payments.services.create_stripe_session import StripePaymentService
 
@@ -34,9 +32,4 @@ def create_payment(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Borrowing)
 def borrowing_created(sender, instance, created, **kwargs):
     if created:
-        try:
-            BorrowingNotificationService().send_borrowing_notification(
-                instance
-            )
-        except Exception as e:
-            print(f"Telegram notification failed: {e}")
+        send_borrowing_notification_task.delay(instance.id)
