6584edcffccf487983df7e6374f3113346dbc492
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-09-23 13:46:45 +0300

    implement check borrowings

--- borrowings/services/borrowing_notification_service.py
+++ borrowings/services/borrowing_notification_service.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.conf import settings
 
 from borrowings.models import Borrowing
@@ -21,4 +22,40 @@ class BorrowingNotificationService(TelegramBotService):
 
     def send_borrowing_notification(self, borrowing: Borrowing) -> None:
         text = self._format_borrowing_notification(borrowing)
+        self.send_notification(settings.TELEGRAM_ADMIN_CHAT_ID, text=text)
+
+    def check_overdue_borrowings(self) -> None:
+        overdue_borrowings = Borrowing.objects.filter(
+            actual_return_date__isnull=True,
+            expected_return_date__lte=timezone.localdate(),
+        ).select_related("book", "user")
+
+        if overdue_borrowings.exists():
+            borrowings_by_user = {}
+
+            for borrowing in overdue_borrowings:
+                borrowings_by_user.setdefault(
+                    borrowing.user.email, []
+                ).append(
+                    borrowing
+                )
+
+            text_parts = []
+            for user, borrowings in borrowings_by_user.items():
+                count_borrowings = len(borrowings)
+                user_text = (
+                    f"{user} has {count_borrowings} "
+                    f"borrowing{'s' if count_borrowings > 1 else ''}:\n"
+                )
+                borrowings_text = "\n".join(
+                    f"'{borrowing.book.title}' "
+                    f"{borrowing.borrow_date}-{borrowing.expected_return_date}"
+                    for borrowing in borrowings
+                )
+                text_parts.append(user_text + borrowings_text)
+
+            text = "\n".join(text_parts)
+        else:
+            text = "No borrowings overdue today!"
+
         self.send_notification(settings.TELEGRAM_ADMIN_CHAT_ID, text=text)
--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,6 @@
 import os
-
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
@@ -9,3 +9,13 @@ app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender: Celery, **kwargs):
+    from borrowings.tasks import check_overdue_borrowings_task
+
+    sender.add_periodic_task(
+        crontab(minute="*/2"),
+        check_overdue_borrowings_task.s(),
+        name="Daily check of overdue borrowings",
+    )
