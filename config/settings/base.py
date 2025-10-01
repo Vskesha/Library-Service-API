9cc1c8227fe5ffb82ddf26ea3e2c8d9774bb1c49
Dmytro Shevchuk <shevchukkdmytro@gmail.com>
2025-10-01 15:19:20 +0300

    fix formatting

--- config/settings/base.py
+++ config/settings/base.py
@@ -144,9 +144,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "books.permissions.IsAdminOrReadOnly"
-    ],
+    "DEFAULT_PERMISSION_CLASSES": ["books.permissions.IsAdminOrReadOnly"],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
     ],
